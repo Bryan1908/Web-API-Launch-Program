4698188faf25b5bb9b0345a4bd6ec9118b09f5d6
Bryan <Bryan@DESKTOP-VT7NU1M>
2019-09-23 11:02:44 +0800

    Add project files.

---
+++ External Program For Web API/Home.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="ExtProgramForWebAPI.Home" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+
+
+  
+
+</head>
+<body>
+    <div>
+
+        <form id="form1" runat="server">
+            Enter data to be posted here:<br />
+            <asp:TextBox ID="tb_PostEntry" runat="server"></asp:TextBox>
+            <br />
+            <asp:Button ID="btn_Post" runat="server" OnClick="btn_Post_Click" Text="POST Data" />
+            <br />
+            <asp:Label ID="lbl_PostResult" runat="server"></asp:Label>
+            <br />
+            <br />
+            <br />
+
+            <asp:Button ID="btn_GetData" runat="server" Text="Get Data" OnClick="btn_GetData_Click" />
+
+
+
+            <br />
+            <asp:Label ID="lbl_RetrievedData" runat="server"></asp:Label>
+            <br />
+            <br />
+            <br />
+            <asp:Button ID="btn_DeleteData" runat="server" OnClick="btn_DeleteData_Click" Text="Delete Data in API" />
+            <br />
+            <asp:Label ID="lbl_Delete" runat="server"></asp:Label>
+            <br />
+            
+        </form>
+    </div>
+</body>
+</html>
---
+++ External Program For Web API/Home.aspx.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Net.Http;
+using System.Web.Http;
+using System.Net.Http.Headers;
+using System.Collections;
+using System.Configuration;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Text;
+using System.IO;
+
+namespace ExtProgramForWebAPI
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        // Link to your API project.
+        static string apiLink = "http://localhost:24497/api/";
+
+        // This triggers the DELETE function in the API.
+        public void deleteDataRequest()
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri(apiLink);
+            var responseTask = client.DeleteAsync("values");
+            responseTask.Wait();
+            var result = responseTask.Result;
+            if(string.Equals(result, "OK"))
+            {
+                lbl_Delete.Text = "Delete Successful;";
+            }else
+            {
+                lbl_Delete.Text = "Delete Failed.";
+            }
+        }
+
+        protected void btn_Post_Click(object sender, EventArgs e)
+        {
+            PostData();
+        }
+
+        // This triggers the POST function in the API.
+        private void PostData()
+        {
+            string dataToBeSent = tb_PostEntry.Text.Trim();
+            var client = new HttpClient();
+            client.BaseAddress = new Uri(apiLink);
+            var postTask = client.PostAsJsonAsync<string>("values?data=" + dataToBeSent, "PostAuth");
+            postTask.Wait();
+            var result = postTask.Result;
+            string resultStr = postTask.Result.ReasonPhrase.ToString();
+            if (resultStr == "OK")
+            {
+                var readTask = result.Content.ReadAsAsync<string>();
+                readTask.Wait();
+                string retStr = readTask.Result.ToString();
+                lbl_PostResult.Text = retStr;
+            }
+        }
+        
+        protected void btn_GetData_Click(object sender, EventArgs e)
+        {
+            string data = "";
+
+            data = getDataRequest();
+            if (string.Equals(data, ""))
+            {
+                lbl_RetrievedData.Text = "No data has been retrieved.";
+            }
+            else
+            {
+                lbl_RetrievedData.Text = data;
+            }
+        }
+
+        // This triggers the GET function in the API.
+        public string getDataRequest()
+        {
+            var data = "";
+            var client = new HttpClient();
+            client.BaseAddress = new Uri(apiLink);
+            var responseTask = client.GetAsync("values");
+            responseTask.Wait();
+            string resultStr = responseTask.Result.ReasonPhrase.ToString();
+            if (resultStr == "OK")
+            {
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<string>();
+                    readTask.Wait();
+
+                    data = readTask.Result;
+                    return data;
+                }
+            }
+            else
+            {
+                return "";
+            }
+
+            return data;
+        }
+
+        protected void btn_DeleteData_Click(object sender, EventArgs e)
+        {
+            deleteDataRequest();
+        }
+    }
+}
